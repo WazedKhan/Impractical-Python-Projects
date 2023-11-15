b1ff7afa37f6bfd1c44163b3004368fc358eaf49
WazedKhan <wazedkhan119399@gmail.com>
2023-11-15 21:24:00 +0600

    use of c profiler

--- 02-Finding_Palingrams/palindromes.py
+++ 02-Finding_Palingrams/palindromes.py
@@ -2,15 +2,18 @@
 
 import load_dictionary
 
-# get and store list of words from text file in a list
-word_list = load_dictionary.load("single_words.txt")
-palindrome_list = []
+def palindrome():
+    # get and store list of words from text file in a list
+    word_list = load_dictionary.load("single_words.txt")
+    palindrome_list = []
 
-# iter over each word and check if we reverse it dose it match with same order
-# [::-1] revers a string by string slicing
-for word in word_list:
-    # if match then include in the palindrome_list
-    if word == word[::-1]:
-        palindrome_list.append(word)
+    # iter over each word and check if we reverse it dose it match with same order
+    # [::-1] revers a string by string slicing
+    for word in word_list:
+        # if match then include in the palindrome_list
+        if word == word[::-1]:
+            palindrome_list.append(word)
 
-print(f"Number of palindrome found: {len(palindrome_list)}")
+    print(f"Number of palindrome found: {len(palindrome_list)}")
+
+palindrome()
